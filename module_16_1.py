acaafcb5b6e1ed48b9e8c66034bb3df68854a033
klaviwka <klaviwka@mail.ru>
2024-12-22 18:47:07 +0300

    Add files via upload

--- module_16_1.py
+++ module_16_1.py
@@ -12,14 +12,14 @@ async def Get_Main_Page():
 # Маршрут для страницы администратора
 @app.get("/user/admin", tags=["default"])
 async def Get_Admin_Page():
-    return JSONResponse(content={"message": "Вы вошли как администратор"})
+    return "Вы вошли как администратор"
 
 # Маршрут для страницы пользователя с параметром в пути
 @app.get("/user/{user_id}", tags=["default"])
 async def Get_User_Number(user_id: int):
-    return JSONResponse(content={"message": f"Вы вошли как пользователь № {user_id}"})
+    return f"Вы вошли как пользователь № {user_id}"
 
 # Маршрут для получения информации о пользователе с параметрами в адресной строке
 @app.get("/user", tags=["default"])
 async def Get_User_info(username: str, age: int):
-    return JSONResponse(content={"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"})
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
