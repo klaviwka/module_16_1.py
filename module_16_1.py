f37b4ddc856005941464da7ea8b72c50381b6478
klaviwka <klaviwka@mail.ru>
2024-12-22 18:59:23 +0300

    Add files via upload

--- module_16_1.py
+++ module_16_1.py
@@ -1,25 +1,24 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
 
 # Создание экземпляра приложения FastAPI
 app = FastAPI()
 
 # Маршрут для главной страницы
 @app.get("/", tags=["default"])
-async def Get_Main_Page():
+async def get_main_page():
     return "Главная страница"
 
 # Маршрут для страницы администратора
 @app.get("/user/admin", tags=["default"])
-async def Get_Admin_Page():
+async def get_admin_page():
     return "Вы вошли как администратор"
 
 # Маршрут для страницы пользователя с параметром в пути
 @app.get("/user/{user_id}", tags=["default"])
-async def Get_User_Number(user_id: int):
+async def get_user_number(user_id: int):
     return f"Вы вошли как пользователь № {user_id}"
 
 # Маршрут для получения информации о пользователе с параметрами в адресной строке
 @app.get("/user", tags=["default"])
-async def Get_User_info(username: str, age: int):
+async def get_user_info(username: str, age: int):
     return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
